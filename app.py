735a0ecf0edf9f064f7576c3ee05661f9bf048d7
krishnasanaka <krishna.sanaka@reisystems.com>
2024-02-16 13:35:00 -0500

    flaskapp_testing

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import sys
+##from harvester.database.interface import HarvesterDBInterface
+#from harvester.database import init_db
+#from tests.database.data import new_source, new_job, new_error
+
+app = Flask(__name__)
+#db = HarvesterDBInterface()
+
+@app.route('/', methods=['GET'])
+def index():
+  return "hello : tesrt program"
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug = True,port=8080)
