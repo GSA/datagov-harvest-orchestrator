c2669b9a843218e73c77fa620b2d0c87fec34eb7
krishnasanaka <krishna.sanaka@reisystems.com>
2024-02-22 11:40:39 -0500

    test

--- app.py
+++ app.py
@@ -1,17 +1,96 @@
 from flask import Flask, request, jsonify
 import sys
-##from harvester.database.interface import HarvesterDBInterface
-#from harvester.database import init_db
-#from tests.database.data import new_source, new_job, new_error
+from harvester.database.interface import HarvesterDBInterface
+from harvester.database import init_db
+from tests.database.data import new_source, new_job, new_error
 
 app = Flask(__name__)
-#db = HarvesterDBInterface()
-``
+db = HarvesterDBInterface()
+
 @app.route('/', methods=['GET'])
 def index():
-  return "hello : test program"
+    html = "<h1>Available Testings</h1>"
+    html += "<ul>"
+    for rule in app.url_map.iter_rules():
+        if 'static' not in rule.endpoint:
+            html += f"<li>{rule.endpoint} : <a href='{rule.rule}'>{rule.rule}</a></li><br>"
+    html += "</ul>"
+    return html
 
+@app.route('/init_db', methods=['GET'])
+def create_tables():
+    result = init_db.create_tables()
+    return result
 
+@app.route('/add_source', methods=['GET'])
+def add_harvest_source():
+    source=db.add_harvest_source(new_source)
+    return(f"Added new source with ID: {source.id}")
+
+@app.route('/add_source', methods=['POST'])
+def add_harvest_source():
+    source=db.add_harvest_source(new_source)
+    return(f"Added new source with ID: {source.id}")
+
+@app.route('/add_job', methods=['GET'])
+def add_harvest_job():
+    source_id = request.args.get('source_id', None)
+    if source_id is None:
+        return 'Please provide source_id'
+    else:
+        job=db.add_harvest_job(new_job, source_id)
+        return(f"Added new job with ID: {job.id}")
+
+@app.route('/add_error', methods=['GET'])
+def add_harvest_error():
+    job_id = request.args.get('job_id', None)
+    if job_id is None:
+        return 'Please provide job_id'
+    else:
+        err=db.add_harvest_error(new_error, job_id)
+        return(f"Added new error with ID: {err.id}")
+    
+@app.route('/get_all_sources', methods=['GET'])
+def get_all_harvest_sources():
+    result = db.get_all_harvest_sources()
+    return result
+
+@app.route('/get_all_jobs', methods=['GET'])
+def get_all_harvest_jobs():
+    result = db.get_all_harvest_jobs()
+    return result
+
+@app.route('/get_all_errors', methods=['GET'])
+def get_all_harvest_errors():
+    result = db.get_all_harvest_errors()
+    return result
+
+@app.route('/get_source', methods=['GET'])
+def get_harvest_source():
+    id = request.args.get('id', None)
+    if id is None:
+        return 'Please provide id'
+    else:
+        result = db.get_harvest_source(id)
+        return result
+    
+@app.route('/get_job', methods=['GET'])
+def get_harvest_job():
+    id = request.args.get('id', None)
+    if id is None:
+        return 'Please provide id'
+    else:
+        result = db.get_harvest_job(id)
+        return result
+
+@app.route('/get_error', methods=['GET'])
+def get_harvest_error():
+    id = request.args.get('id', None)
+    if id is None:
+        return 'Please provide id'
+    else:
+        result = db.get_harvest_error(id)
+        return result
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug = True,port=8080)
+    app.run(host="0.0.0.0", port=8080)
--- create_tables_invokable1.py
+++ create_tables_invokable1.py
@@ -2,24 +2,25 @@ import psycopg2
 from psycopg2 import sql
 
 def initialize_tables(connection):
+  
     cursor = connection.cursor()
 
     # Add your table creation SQL statements here
     table_creation_queries = [
         """
         CREATE TABLE IF NOT EXISTS harvest_source (
-        name String,
-        notification_emails String,
-        organization_name String,
-        frequency String,
+        name varchar(100),
+        notification_emails varchar(100),
+        organization_name varchar(100),
+        frequency varchar(100),
         config json
         );
         """,
         """
         CREATE TABLE IF NOT EXISTS harvest_job(
         harvest_source_id UUID,
-        date_created DateTime,
-        date_finished DateTime,
+        date_created timestamp,
+        date_finished timestamp,
         records_added Integer,
         records_updated Integer,
         records_deleted Integer,
@@ -29,12 +30,12 @@ def initialize_tables(connection):
          """
         CREATE TABLE IF NOT EXISTS harvest_error(
         harvest_job_id UUID,
-        record_id String,
-        record_reported_id String,
-        date_created DateTime,
-        type String,
-        severity String,
-        message String);
+        record_id varchar(100),
+        record_reported_id varchar(100),
+        date_created timestamp,
+        type varchar(100),
+        severity varchar(100),
+        message varchar(100));
         """
         # Add more table creation statements as needed
     ]
@@ -47,14 +48,17 @@ def initialize_tables(connection):
 
 if __name__ == "__main__":
     # Modify the connection parameters according to your database configuration
-
-    connection_details="postgresql+psycopg2://login:pass@localhost/test"
     try:
-        connection = psycopg2.connect(connection_details)
+        connection = psycopg2.connect(
+        port="5432",
+        dbname="postgres",
+        user="postgres",
+        host="localhost",
+        password="Shanu123")
         initialize_tables(connection)
         print("Tables initialized successfully.")
     except Exception as e:
-        print(f"Error: {e}")
+        print(e)
     finally:
         if connection:
             connection.close()
