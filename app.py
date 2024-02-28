c02d6272b260e91eeae3a3ad8ff6aa6cebb6497e
krishnasanaka <krishna.sanaka@reisystems.com>
2024-02-28 11:35:30 -0500

    Implementeed logic GET for all records and single record

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, jsonify
 import psycopg2
 
@@ -28,8 +29,8 @@ def insert_data():
         schema_type = request.form['schema_type']
         source_type  = request.form['source_type']
         print(name,notification_emails,orgnation_name,frequency,url,schema_type,source_type)
-        cursor.execute("INSERT INTO public.harvest_source(name,notification_emails,organization_name,frequency,schema_type,source_type) VALUES (%s,%s,%s,%s,%s,%s)", (name,notification_emails,orgnation_name,frequency,schema_type,source_type))
-        #cursor.execute("INSERT INTO public.harvest_source(name,notification_emails,organization_name,frequency,url,schema_type,source_type) VALUES (%s,%s, %s,%s,%s,%s,%s)", (name,notification_emails,orgnation_name,frequency,url,schema_type,source_type))
+        #cursor.execute("INSERT INTO public.harvest_source(name,notification_emails,organization_name,frequency,schema_type,source_type) VALUES (%s,%s,%s,%s,%s,%s)", (name,notification_emails,orgnation_name,frequency,schema_type,source_type))
+        cursor.execute("INSERT INTO public.harvest_source(name,notification_emails,organization_name,frequency,url,schema_type,source_type) VALUES (%s,%s, %s,%s,%s,%s,%s)", (name,notification_emails,orgnation_name,frequency,url,schema_type,source_type))
         conn.commit()
         cursor.close()
         return jsonify({'message': 'Data inserted successfully'})
@@ -39,12 +40,18 @@ def insert_data():
 @app.route('/harvests', methods=['GET'])
 def get_record():
     try:
+        print("test12233")
         cursor = conn.cursor()
         record_id = request.args.get('id')
-        if record_id :
-            cursor.execute("SELECT * FROM harvest_source WHERE id = %s", (id,))
-        cursor.execute("SELECT * FROM harvest_source")
+        print("record_id",record_id)
+        #param_value = request.args.get('param')
+        if record_id is not None :
+            query = "SELECT * FROM harvest_source WHERE id = %s"
+            cursor.execute(query, (record_id,))
+        else:
+            cursor.execute("SELECT * FROM harvest_source")
         record = cursor.fetchall()
+        print("test",record)
         cursor.close()
         if record:
             return jsonify({'record': record})
