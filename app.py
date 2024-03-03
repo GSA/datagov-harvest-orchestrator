a6439cba722715f42563d27428e1cea80f064296
krishnasanaka <krishna.sanaka@reisystems.com>
2024-03-02 23:15:02 -0500

    flask_harvester_app changes

--- app.py
+++ app.py
@@ -82,6 +82,47 @@ def get_harvest_error():
         result = db.get_harvest_error(id)
         return result
 
+@app.route('/harvests/new')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def insert_data():
+    try:
+        cursor = db.cursor()
+        name = request.form['name']
+        notification_emails = request.form['notification_emails']
+        orgnation_name = request.form['orgnation_name']
+        frequency = request.form['frequency']
+        url = request.form['url']
+        schema_type = request.form['schema_type']
+        source_type  = request.form['source_type']
+        cursor.execute("INSERT INTO public.harvest_source(name,notification_emails,organization_name,frequency,url,schema_type,source_type) VALUES (%s,%s, %s,%s,%s,%s,%s)", (name,notification_emails,orgnation_name,frequency,url,schema_type,source_type))
+        db.commit()
+        cursor.close()
+        return jsonify({'message': 'Data inserted successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/harvests', methods=['GET'])
+def get_record():
+    try:
+        cursor = db.cursor()
+        record_id = request.args.get('id')
+        if record_id is not None :
+            query = "SELECT * FROM harvest_source WHERE id = %s"
+            cursor.execute(query, (record_id,))
+        else:
+            cursor.execute("SELECT * FROM harvest_source")
+        record = cursor.fetchall()
+        cursor.close()
+        if record:
+            return jsonify({'record': record})
+        else:
+            return jsonify({'error': 'No records found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db.close()
